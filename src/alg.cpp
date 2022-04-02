00258921080e158b3702bda78c2269c727a5731a
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-04-02 13:47:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,15 +28,17 @@ int prior(char ch) {
 
 std::string infx2pstfx(std::string inf) {
   // добавьте код
-TStack <char, 100> stack1;
 std::string post;
+TStack <char, 100> stack1;
 for (int i = 0; i < inf.size(); i++) {
     if (prior(inf[i]) == -1) {
         post.push_back(inf[i]);
         post.push_back(' ');
-    } else if (prior(inf[i]) == 0 || stack1.isEmpty() || prior(inf[i]) > prior(stack1.get())) {
+    } else if (prior(inf[i]) == 0 || stack1.isEmpty()) {
             stack1.push(inf[i]);
-        } else if (prior(inf[i]) == 1) {
+        } else if (prior(inf[i]) > prior(stack1.get())) {
+            stack1.push(inf[i]);
+    }else if (prior(inf[i]) == 1) {
             while (prior(stack1.get()) != 0) {
                 post.push_back(stack1.get());
                 post.push_back(' ');
